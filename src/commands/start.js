691859b57760dc6c34c254471f36d5caec76f17a
sotiris balas <sotirismpalas@gmail.com>
2024-01-04 20:15:47 +0200

    removed console log

--- src/commands/start.js
+++ src/commands/start.js
@@ -2,6 +2,5 @@ import { bot, options } from "../config.js";
 const chatId = Number(process.env.CHAT_ID);
 
 export const start = () => {
-  console.log(chatId);
   bot.sendMessage(chatId, "Choose one of the following commands: ✅", options);
 };
