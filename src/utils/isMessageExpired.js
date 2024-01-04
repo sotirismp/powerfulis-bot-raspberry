7f0856b0b66fb54de2fbe66b15f4e5badabf9175
sotiris balas <sotirismpalas@gmail.com>
2024-01-04 19:17:50 +0200

    added const to a variable

--- src/utils/isMessageExpired.js
+++ src/utils/isMessageExpired.js
@@ -1,4 +1,6 @@
+const EXPIRED_MESSAGE_THRESHOLD_IN_SECONDS = 5;
+
 exports.isMessageExpired = (date) => {
   const DateNow = Math.floor(Date.now() / 1000);
-  return DateNow - date > 5 ? true : false;
+  return DateNow - date > EXPIRED_MESSAGE_THRESHOLD_IN_SECONDS ? true : false;
 };
